18d7ec6f68ee96e376766d6ff0a8ae99e4643eea
21mh1a0412 <97926751+21mh1a0412@users.noreply.github.com>
2022-01-29 17:59:14 +0530

    #include<stdio.h> int main() {     int num;     int rem=0,sum=0,n;     scanf("%d",&num);     //make a copy of num and store it in varible n     n=num;     //calculate sum of digits     while(num>0)     {        rem=num%10;        sum=sum+rem;        num=num/10;     }     //check wheather number is divisible by sum of digits     if(n%sum==0)     printf("True");     else     printf("False");     return 0; muk
    }

---
+++ Harshed_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    int rem=0,sum=0,n;
+    scanf("%d",&num);
+    //make a copy of num and store it in varible n
+    n=num;
+    //calculate sum of digits
+    while(num>0)
+    {
+       rem=num%10;
+       sum=sum+rem;
+       num=num/10;
+    }
+    //check wheather number is divisible by sum of digits
+    if(n%sum==0)
+    printf("True");
+    else
+    printf("False");
+    return 0; muk
+    
+}
